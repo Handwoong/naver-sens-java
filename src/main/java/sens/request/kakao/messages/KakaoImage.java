ce523a984bdb0393ac15c918f3182aa00a7d2f1e
handwoong <handwoong@gmail.com>
2023-03-05 20:11:56 +0900

    chore: 버전 수정

--- src/main/java/sens/request/kakao/messages/KakaoImage.java
+++ src/main/java/sens/request/kakao/messages/KakaoImage.java
@@ -1,4 +1,19 @@
 package sens.request.kakao.messages;
 
-public record KakaoImage(String imageId, String imageLink) {
+public class KakaoImage {
+	private final String imageId;
+	private final String imageLink;
+
+	public KakaoImage(String imageId, String imageLink) {
+		this.imageId = imageId;
+		this.imageLink = imageLink;
+	}
+
+	public String getImageId() {
+		return imageId;
+	}
+
+	public String getImageLink() {
+		return imageLink;
+	}
 }
--- src/main/java/sens/request/kakao/messages/KakaoItemTitleAndDescription.java
+++ src/main/java/sens/request/kakao/messages/KakaoItemTitleAndDescription.java
@@ -1,4 +1,19 @@
 package sens.request.kakao.messages;
 
-public record KakaoItemTitleAndDescription(String title, String description) {
+public class KakaoItemTitleAndDescription {
+	private final String title;
+	private final String description;
+
+	public KakaoItemTitleAndDescription(String title, String description) {
+		this.title = title;
+		this.description = description;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 }
